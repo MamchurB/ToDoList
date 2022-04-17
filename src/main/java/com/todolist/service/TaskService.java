add4cf67440d7b98b299877e425baa96512e69f1
BasyMan <picbasyman@gmail.com>
2022-04-17 10:36:49 +0300

    Add Entity Task

---
+++ src/main/java/com/todolist/service/TaskService.java
@@ -0,0 +1,25 @@
+package com.todolist.service;
+
+import com.todolist.model.Role;
+import com.todolist.model.Task;
+import com.todolist.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface TaskService {
+
+    List<Task> userList();
+
+    Page<Task> findAll(Pageable pageable);
+
+    Task findOne(Long id);
+
+    String addTask(Task user);
+
+    String deleteTask(Long id);
+
+
+
+}
---
+++ src/main/java/com/todolist/service/impl/TaskServiceImpl.java
@@ -0,0 +1,68 @@
+package com.todolist.service.impl;
+
+import com.todolist.model.Task;
+import com.todolist.model.User;
+import com.todolist.repository.TaskRepository;
+import com.todolist.service.TaskService;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
+
+public class TaskServiceImpl implements TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+
+    @Override
+    public List<Task> userList() {
+        return taskRepository.taskList();
+    }
+
+    @Override
+    public Page<Task> findAll(Pageable pageable) {
+        return taskRepository.findAll(new PageRequest(pageable.getPageNumber() - 1, 5));
+    }
+
+    @Override
+    public Task findOne(Long id) {
+        return taskRepository.findOne(id);
+    }
+
+    @Override
+    public String addTask(Task task) {
+        String message = null;
+        JSONObject jsonObject = new JSONObject();
+        try {
+            if(task.getTaskId() == null) {
+                message = "Added";
+            } else {
+                message = "Updated";
+            }
+            jsonObject.put("status", "success");
+            jsonObject.put("title", message+" Confirmation");
+            jsonObject.put("message", taskRepository.save(task).getTaskName()+" "+message+" successfully.");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
+    }
+
+    @Override
+    public String deleteTask(Long id) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            taskRepository.delete(id);
+            jsonObject.put("message", "Task Deleted Successfully.");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject.toString();
+    }
+}
